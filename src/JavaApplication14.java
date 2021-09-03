efe1a36819b74b64d65a4c079777341c404ed2fa
bpavel07@yandex.ru <gggtttttHHHHHH7889787>
2021-09-03 14:25:10 +0300

    FileWriter

--- src/JavaApplication14.java
+++ src/JavaApplication14.java
@@ -1,9 +1,22 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class JavaApplication14 {
 
     public static void main(String[] args) {
 
-        
+        writeHelloToFile("hello.txt");
 
+
+    }
+
+    private static void writeHelloToFile(String s) {
+
+        try (FileWriter writer = new FileWriter("hellotxt")) {
+                writer.write("Привет из Java !");
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
 
     }
 
